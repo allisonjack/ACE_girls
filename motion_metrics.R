dc423ea6aa2b51518f6ed4b7502c7c24ac05e2e6
Allison Jack <allison.e.jack@gmail.com>
2014-10-13 14:30:50 -0400

    Update motion_metrics.R

--- motion_metrics.R
+++ motion_metrics.R
@@ -34,3 +34,47 @@ rms.df <- merge(rms.abs.summ, rms.rel.summ, by = c("ID"))
 
 #Write this dataframe to a .csv file
 write.csv(rms.df, sprintf("%s/%s/rms_df.csv", STUDYDIR, DIFFDIR), row.names=FALSE)
+
+#Read in a motion outliers count file.
+mo.df <- read.table(sprintf("%s/%s/motion_outliers_count.txt", STUDYDIR, DIFFDIR),
+                    header = TRUE)
+
+df <- merge(rms.df, mo.df, by = "ID")
+
+#---Specify inclusion cutoffs---#
+#These can be whatever you think is appropriate. Here I specify that 
+#a participant cannot have more than 20% of total volumes be identified
+#as motion outliers, and that the RMS motion cutoff is 4 mm. 
+
+Total.vols <- 172
+MO.cutoff <- .2*Total.vols
+RMS.cutoff <- 4
+
+#---Check RMS values---#
+
+df$RMS.abs.include[rms.df$rms.abs.max>=RMS.cutoff] <- "no"
+df$RMS.abs.include[rms.df$rms.abs.max<RMS.cutoff] <- "yes"
+df$RMS.rel.include[rms.df$rms.rel.max>=RMS.cutoff] <- "no"
+df$RMS.rel.include[rms.df$rms.rel.max<RMS.cutoff] <- "yes"
+
+#---Check number of outliers (according to DVARS)---#
+
+df$MO.include[df$OutlierCount>=MO.cutoff] <- "no"
+df$MO.include[df$OutlierCount<MO.cutoff] <- "yes"
+
+#---Decision rule---#
+
+df$Include[df$RMS.abs.include =="no" | df$RMS.rel.include == "no" | df$MO.include == "no"] <- "no"
+df$Include[df$RMS.abs.include == "yes" & df$RMS.rel.include == "yes" & df$MO.include == "yes"] <- "yes"
+
+#Create a .csv file with all the information in the current dataframe.
+write.csv(df, sprintf("%s/%s/motion.csv", STUDYDIR, DIFFDIR), row.names=FALSE)
+
+#Create a .txt file with just the ID numbers of participants who "passed inspection"
+df.include <- subset(df, df$Include == "yes", select = ID)
+write.table(df.include, 
+            sprintf("%s/%s/IDs_include.txt", STUDYDIR, DIFFDIR),
+            row.names = FALSE,
+            quote     = FALSE,
+            col.names = FALSE)
+            
