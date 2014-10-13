3b33b5071b02b4afc748b537fbad6b8c4ac3e3e8
Allison Jack <allison.e.jack@gmail.com>
2014-10-13 14:03:52 -0400

    Update motion_metrics.R

--- motion_metrics.R
+++ motion_metrics.R
@@ -9,15 +9,15 @@
 STUDYDIR <- "/Volumes/HIPPOCAMPUS/ACE_girls"
 DIFFDIR <- "/ANALYSIS/INDIV_DIFF"
 
-#rms <- read.table(sprintf("%s/%s/rms.txt", STUDYDIR, DIFFDIR), 
-                  header     = T,
-                  fill       = T,
+#Read in the rms.txt file. Respect the header. Fill in empty cells with "NA."
+rms <- read.table(sprintf("%s/%s/rms.txt", STUDYDIR, DIFFDIR), 
+                  header     = TRUE,
+                  fill       = TRUE,
                   na.strings = "")
 
 #---Load packages---#
 #Make sure you have installed the package first
 library(plyr)
-
 
 #---Summarize RMS data by participant
 rms.abs.summ <- ddply(rms, .(ID), summarise,
@@ -29,6 +29,8 @@ rms.rel.summ <- ddply(rms, .(ID), summarise,
                       rms.rel.SD  = sd(REL, na.rm = TRUE),
                       rms.rel.max = max(REL, na.rm = TRUE),
                       rms.rel.min = min(REL, na.rm = TRUE))
+#Create a new dataframe with the summary info
 rms.df <- merge(rms.abs.summ, rms.rel.summ, by = c("ID"))
 
+#Write this dataframe to a .csv file
 write.csv(rms.df, sprintf("%s/%s/rms_df.csv", STUDYDIR, DIFFDIR), row.names=FALSE)
